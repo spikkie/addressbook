408950100357c61b9670c135ced18a8cdbc91647
Bas <spikkie@gmail.com>
2018-01-14 23:55:45 +0100

    Initial Postsi 2

--- addressbook/urls.py
+++ addressbook/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 """
 from django.conf import settings
 from django.urls import include
-from django.conf.urls import url, static
+from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 
@@ -28,6 +29,6 @@ urlpatterns = [
 ]
 
 #TODO
-#if settings.DEBUG:
-#    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

80da36c64c9192639e31f07f931e1d8663a281d4
Bas <spikkie@gmail.com>
2018-01-14 17:26:07 +0100

    set env TIMES 2

--- hello/views.py
+++ hello/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+import os
 import requests
 
 # Create your views here.
@@ -10,10 +11,15 @@ import requests
 #    # return HttpResponse('Hello from Python!')
 #    return render(request, 'index.html')
 
+#def index(request):
+#    r = requests.get('http://httpbin.org/status/418')
+#    print(r.text)
+#    return HttpResponse('<pre>' + r.text + '</pre>')
+
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    times = int(os.environ.get('TIMES',3))
+    return HttpResponse('Hello! ' * times)
+
 
 
 

578f4b6dd67e69be222f023dfc6158b7396e7384
Bas <spikkie@gmail.com>
2018-01-16 02:14:21 +0100

    Posts 16 jan 1 LOGGING  // static/css/base.css

--- addressbook/settings.py
+++ addressbook/settings.py
@@ -141,3 +141,22 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
 
 django_heroku.settings(locals())
 
+
+import os
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
+
